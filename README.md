36554192101f8f7b2f9d55c9bd0de84cebdd8e33
ashneetrathore <ashneetrathore@gmail.com>
2026-01-27 18:27:29 -0800

    publishing db design project

---
+++ README.md
@@ -0,0 +1,15 @@
+# :weight_lifting: THE ARC DATABASE DESIGN
+
+## :open_book: OVERVIEW
+Date: October 2023\
+Designer(s): Ashneet Rathore, Guillermo Bolger\
+Based on assignment instructions from Prof. Mary Roth
+
+This repository presents a relational database design for the Anteater Recreation Center (ARC), a gym located on the University of California, Irvine campus. The purpose of the database is to support core gym operations such as member management, event enrollment, employee scheduling, and facility usage. The project consists of three main parts: an **entity relationship diagram (ERD)**, a **relational notation** document, and a **schema** implementation with a **MySQL** script. 
+
+The repo also includes a system requirements narrative that informed the modeling decisions and schema constraints throughout the design process. Certain requirements, such as the presence of sensor technology, were intentionally extended for design exploration purposes and do not necessarily reflect current ARC operations.
+
+
+
+
+
---
+++ arc.sql
@@ -0,0 +1,104 @@
+CREATE TABLE Person (
+id INT PRIMARY KEY,
+first_name VARCHAR(50) NOT NULL,
+last_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Member (
+id INT PRIMARY KEY,
+street_address1 VARCHAR(50) NOT NULL,
+street_address2 VARCHAR(50),
+city VARCHAR(50) NOT NULL,
+zip VARCHAR(10) NOT NULL,
+member_type ENUM('Undergrad_Student', 'Grad_Student', 'Faculty', 'Staff', 'Alumni', 'Family') NOT NULL,
+relation_to INT,
+FOREIGN KEY (id) REFERENCES Person(id),
+FOREIGN KEY (relation_to) REFERENCES Member(id),
+CHECK (
+	(member_type = 'Family' AND relation_to IS NOT NULL)
+	OR
+	(member_type <> 'Family' AND relation_to IS NULL)
+	)
+);
+
+CREATE TABLE Employee (
+id INT PRIMARY KEY,
+designation ENUM('Trainer', 'Desk_Emp') NOT NULL,
+FOREIGN KEY (id) REFERENCES Person(id)
+);
+
+CREATE TABLE EmployeeSchedule(
+shift_id INT AUTO_INCREMENT PRIMARY KEY,
+emp_id INT NOT NULL,
+work_day ENUM('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun') NOT NULL,
+start_time TIME NOT NULL,
+end_time TIME NOT NULL,
+FOREIGN KEY (emp_id) REFERENCES Employee(id)
+);
+
+CREATE TABLE PaymentInfo(
+member_id INT PRIMARY KEY,
+payment_ref VARCHAR(20) NOT NULL,
+FOREIGN KEY (member_id) REFERENCES Member(id)
+);
+
+CREATE TABLE Room (
+id INT PRIMARY KEY,
+name ENUM('Front_Desk', 'Cardio', 'Weight', 'Pool', 'Yoga', 'Basketball_Court', 'Other') NOT NULL,
+max_capacity INT NOT NULL
+);
+
+CREATE TABLE Equipment (
+id INT PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+room_id INT NOT NULL,
+in_use BOOLEAN NOT NULL,
+in_use_by INT,
+FOREIGN KEY (room_id) REFERENCES Room(id),
+FOREIGN KEY (in_use_by) REFERENCES Member(id),
+CHECK(
+	(in_use = TRUE and in_use_by IS NOT NULL)
+	OR
+	(in_use = FALSE and in_use_by IS NULL)
+	)
+);
+
+CREATE TABLE EntryLog (
+person_id INT,
+entry_time TIMESTAMP,
+PRIMARY KEY (person_id, entry_time),
+FOREIGN KEY (person_id) REFERENCES Person(id)
+);
+
+CREATE TABLE EmployeeExitLog (
+emp_id INT,
+exit_time TIMESTAMP,
+PRIMARY KEY (emp_id, exit_time),
+FOREIGN KEY (emp_id) REFERENCES Employee(id)
+);
+
+CREATE TABLE LocationLog(
+person_id INT,
+room_id INT,
+entry_time TIMESTAMP,
+PRIMARY KEY (person_id, room_id, entry_time),
+FOREIGN KEY (person_id) REFERENCES Person(id),
+FOREIGN KEY (room_id) REFERENCES Room(id)
+);
+
+CREATE TABLE Event(
+id INT PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+room_id INT NOT NULL,
+start_time DATETIME NOT NULL,
+end_time DATETIME NOT NULL,
+FOREIGN KEY (room_id) REFERENCES Room(id)
+);
+
+CREATE TABLE EventEnrollment(
+event_id INT,
+member_id INT,
+PRIMARY KEY (event_id, member_id),
+FOREIGN KEY (event_id) REFERENCES Event(id),
+FOREIGN KEY (member_id) REFERENCES Member(id)
+);
